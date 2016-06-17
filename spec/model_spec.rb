a0ddafcda593e2057bc3ec4436788c5ec402e277
Davis, Adam <Christopher.Davis@healthsparq.com>
2016-06-16 22:22:02 -0700

    rookie mistakes, to the left

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -2,11 +2,17 @@ require File.join(File.dirname(File.expand_path(__FILE__)), 'spec_helper')
 require_relative '../model/user'
 
 describe User do
-  describe "get name" do
-    context "given jeremy fixture" do
+  context "given jeremy fixture" do
+    context "get name" do
       it "should be named jeremy" do
         user = load(:user__jeremy)
         expect(user.values[:name].eql? "jeremy")
+      end
+    end
+    context "get password" do
+      it "should be passworded asdf" do
+        user = load(:user__jeremy)
+        expect(user.values[:password].eql? "asdf")
       end
     end
   end
